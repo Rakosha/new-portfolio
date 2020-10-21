2f25a23b1e5058c26dafb7a0f69c3fc4b5f78b64
Rakosha <rakot1997@gmail.com>
2020-10-20 17:44:36 -0700

    v4.1

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Video from '../../videos/video.mp4'
+import video from '../../videos/video.mp4'
 import {Button} from "../ButtonElement"
 import {HeroLinksWrapper,HeroContainer,HeroBg,VideoBg, HeroBtnWrapper,HeroContent, ArrowForward, ArrowRight, HeroH1, HeroP} from "./HeroElemets"
 
@@ -24,7 +24,7 @@ const HeroSection = () => {
     return (
         <HeroContainer>
             <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+                <VideoBg autoPlay loop muted src={video} type='video/mp4'/>
             </HeroBg>
             <HeroContent>
                 <Fade left>
